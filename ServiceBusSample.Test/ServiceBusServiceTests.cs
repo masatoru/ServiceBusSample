01c009342998f574903e7453418c9f0ba2a6893b
Satoru Fujimori <satoru.fujimori@gmail.com>
2018-12-14 15:20:19 +0900

    add ServiceBusServiceTests project

---
+++ ServiceBusSample.Test/ServiceBusServiceTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class ServiceBusServiceTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void GetSubscriptionList()
+        {
+            Assert.AreEqual(true, false);
+        }
+    }
+}
