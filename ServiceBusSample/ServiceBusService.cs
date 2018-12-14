d55eeac94cea4240219c868888675a7ea899c4f5
Satoru Fujimori <satoru.fujimori@gmail.com>
2018-12-14 17:38:20 +0900

    improve ServiceBusService

--- ServiceBusSample/ServiceBusService.cs
+++ ServiceBusSample/ServiceBusService.cs
@@ -4,28 +4,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Azure.Management.ResourceManager.Fluent;
 using Microsoft.Azure.Management.ServiceBus.Fluent;
-using Microsoft.Azure.ServiceBus.Management;
-using ISubscriptions = Microsoft.Azure.Management.ServiceBus.Fluent.ISubscriptions;
 
 namespace ServiceBusSample
 {
     public class ServiceBusService
     {
-        private const string CLIENT_ID = "";
-        private const string CLIENT_SECRET = "";
-        private const string TENANT_ID = "";
-        private const string SUBSCRIPTION_ID = "";
-        private const string RESOURCE_GROUP = "";
-        private const string RESOURCE_NAME = "";
 
         private IServiceBusNamespace serviceBusNamespace { get; set; }
 
         public ServiceBusService()
         {
-            var credentials = SdkContext.AzureCredentialsFactory.FromServicePrincipal(CLIENT_ID, CLIENT_SECRET, TENANT_ID,
+            var credentials = SdkContext.AzureCredentialsFactory.FromServicePrincipal(Constant.CLIENT_ID, Constant.CLIENT_SECRET, Constant.TENANT_ID,
                 AzureEnvironment.AzureGlobalCloud);
-            var serviceBusManager = ServiceBusManager.Authenticate(credentials, SUBSCRIPTION_ID);
-            serviceBusNamespace = serviceBusManager.Namespaces.GetByResourceGroup(RESOURCE_GROUP, RESOURCE_NAME);
+            var serviceBusManager = ServiceBusManager.Authenticate(credentials, Constant.SUBSCRIPTION_ID);
+            serviceBusNamespace = serviceBusManager.Namespaces.GetByResourceGroup(Constant.RESOURCE_GROUP, Constant.RESOURCE_NAME);
         }
 
         /// <summary>
@@ -33,11 +25,13 @@ namespace ServiceBusSample
         /// </summary>
         /// <param name="topicPath"></param>
         /// <returns></returns>
-        public async Task<ISubscriptions> GetSubscriptions(string topicName)
+        public async Task<List<Microsoft.Azure.Management.ServiceBus.Fluent.ISubscription>> GetSubscriptions(string topicName)
         {
             var topics = await serviceBusNamespace.Topics.ListAsync();
             return topics.FirstOrDefault(t => t.Name == topicName)?
-                .Subscriptions;
+                .Subscriptions
+                .List()
+                .ToList();
         }
     }
 }
